9e186b97d69c4c55238ee6aabc1f635e25ffba73
riyanshi2817 <riyanshijoshi8@gmail.com>
2025-03-22 11:37:15 +0530

    commiting assignment

---
+++ java_script/ointro.js
@@ -0,0 +1,54 @@
+// console.log("hello");
+// var , let ,const;
+// var a = 10;
+// var b = "hello";
+// var c = {};
+// var d = [ 1, 2 ,3 ,4];
+// var e = function(a,b){return a+b};
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c );
+// console.log(typeof d );
+// console.log(typeof e );
+
+
+// function fn(a,b)
+// {
+//     return a + b;
+// }
+// console.log(fn(3,5))
+// getName();
+// console.log(x);
+// console.log(getName);
+// var x = 7;
+// var getName2 = function (){
+
+// }
+// function getName() {
+//     console.log("Namaste Javascript");
+// }  
+
+// function a() {
+//     var b = 10;
+
+//     c();
+//     function c() {
+//         // console.log(b);
+//     }
+// }
+// a();
+// console.log(b);
+
+// let c = 0;
+// for (let i=0; i<5; i++)
+// {
+//     console.log(c)
+// }
+let counter = 120;
+console.log(typeof(counter)); // "number"
+
+counter = false; 
+console.log(typeof(counter)); // "boolean"
+
+counter = "Hi";
+console.log(typeof(counter)); // "string"
