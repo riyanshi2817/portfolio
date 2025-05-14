0265733160c5fd4633fc2f02b3cc732819af45cf
riyanshi2817 <riyanshijoshi8@gmail.com>
2025-05-14 11:04:20 +0530

    comitting all files

--- js_toso_list/script.js
+++ js_toso_list/script.js
@@ -1,19 +1,19 @@
-let inputs = document.getElementById("inp");
-let text = document.querySelector(".text");
+// let inputs = document.getElementById("inp");
+// let text = document.querySelector(".text");
 
-function Add(){
-    if(inputs.value == ""){
-        alert("Please Enter task")    
-    }
-    else{
-        let newEle = document.createElement("ul");
-        newEle.innerHTML = `${inputs.value}` <i class="fa-solid fa-trash"></i> ;
-        text.appendChild(newEle); 
-        inputs.value="";
-        newEle.querySelector("i").addEventListener("click" , remove);
-        function remove(){
-            newEle.remove()
-        }
-    }
+// function Add(){
+//     if(inputs.value == ""){
+//         alert("Please Enter task")    
+//     }
+//     else{
+//         let newEle = document.createElement("ul");
+//         newEle.innerHTML = `${inputs.value}` <i class="fa-solid fa-trash"></i> ;
+//         text.appendChild(newEle); 
+//         inputs.value="";
+//         newEle.querySelector("i").addEventListener("click" , remove);
+//         function remove(){
+//             newEle.remove()
+//         }
+//     }
 
-}
+// }
