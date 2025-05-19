2563d8751555789a8c425c89fe09eb927c4b0c78
riyanshi2817 <riyanshijoshi8@gmail.com>
2025-05-19 14:51:11 +0530

    comitting all files

--- todoapp/script.js
+++ todoapp/script.js
@@ -1,1 +1,41 @@
-const 
+const write = document.querySelector(".out");
+const display = document.querySelector(".container");
+const bt1 = document.querySelector(".btn1");
+const bt2 = document.querySelector(".btn2");
+ 
+bt1.addEventListener("click", addtask)
+bt2.addEventListener("click" , clearTasks);
+
+
+function addtask(){
+    const task = write.value.trim();
+
+    if(task===""){
+        alert("Please enter a task!");
+        return;
+    }
+
+   const taskItem = document.createElement("div");
+    taskItem.className = "task-item";
+    taskItem.innerText = task;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "remove";
+    deleteBtn.className = "delete-btn";
+
+    deleteBtn.addEventListener("click", () => {
+        taskItem.remove();
+    });
+
+    taskItem.appendChild(deleteBtn);
+
+    display.appendChild(taskItem);
+
+    write.value = "";
+}
+
+function clearTasks(){
+    display.innerHTML = "";
+}
+
+
