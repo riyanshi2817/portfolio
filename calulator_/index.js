a96f7da5c8d3494e21e0cab8b39023b5614a3f85
riyanshi2817 <riyanshijoshi8@gmail.com>
2025-07-07 02:29:03 +0530

    Calculator

--- calulator_/index.js
+++ calulator_/index.js
@@ -1,1 +1,24 @@
+const input = document.getElementById('inp');
+const btn = document.querySelectorAll('.btn');
+const del = document.querySelector('.del');
+const clear = document.querySelector('.clr');
+const equal = document.querySelector('.equals');
+
+btn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        input.value += btn.textContent
+    })
+});
+
+del.addEventListener("click" , () => {
+    input.value = input.value.slice(0, -1);
+});
+
+clear.addEventListener("click" , () => {
+    input.value = "";
+});
+
+equal.addEventListener("click" , () => {
+    input.value = eval(input.value);
+});
 
