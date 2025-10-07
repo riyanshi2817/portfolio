3834c9a16456e4214864708fcfa640ecc355d577
riyanshi2817 <riyanshijoshi8@gmail.com>
2025-10-07 23:05:04 +0530

    deleted something

--- Myreact/src/App.jsx
+++ Myreact/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import UserCard from './components/UserCard'
+import "./App.css"
+import "./index.css"
 function App() {
   return (
-    <div>
-      
+    <div className='container' >
+      <UserCard name="Riyanshi Joshi" desc="yoyo" style={{"border-radius":"100px"}} />
+      <UserCard name="vidushi Joshi" desc="hello" />
+      <UserCard name="bandar" desc="whatsapp" />
     </div>
   )
 }
