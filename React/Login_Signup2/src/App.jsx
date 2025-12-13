47690364f3539304dceecb58fc6e3d2a1ff5bde2
riyanshi2817 <riyanshijoshi8@gmail.com>
2025-12-14 00:06:57 +0530

    Login Sign up page in react 2

---
+++ React/Login_Signup2/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter,Routes, Route} from 'react-router-dom' 
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Home from './components/Home'
+
+function App() {
+
+
+  return (
+    <>
+     <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Signup/>} /> 
+          <Route path='/login' element={<Login/>} />  
+          <Route path='/home' element={<Home/>} />  
+
+        </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ React/Login_Signup2/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <a className='heading'>Explore!</a>
+                    <a>Signup</a>
+                    <a>Login</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
